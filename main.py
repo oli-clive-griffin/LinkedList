48dc77a9696f3077bbc080fc72658ae21497b795
Oli Clive-Griffin <o.clivegriffin@gmail.com>
2021-05-20 23:45:00 +1200

    added reversing and more tests, refactor

--- main.py
+++ main.py
@@ -11,68 +11,97 @@ class Linked_List:
     else:
       self.head = Node(e)
   
+  # time complexity: O(1)
   def add_first(self, e):
-    if self.head == None:
-      self.head = Node(e)
-      return
-    temp = Node(e, self.head)
-    self.head = temp
+    if not isinstance(e, int):
+      raise Exception("wrong data type")
 
+    self.head = Node(e, self.head)
+
+  # time complexity: O(n)
   def add_last(self, e):
-    if self.head == None:
-      self.head = Node(e)
-      return
-    current = self.head
-    while(current.next):
-      current = current.next
-    current.next = Node(e)
+    if not isinstance(e, int):
+      raise Exception("wrong data type")
 
+    current = self.head
+    if current == None:
+      self.head = Node(e)
+    else:
+      while(current.next):
+        current = current.next
+      current.next = Node(e)
+
+  # time complexity: O(1)
+  def remove_first(self):
+    current = self.head
+    if current == None:
+      raise Exception("list is empty")
+
+    self.head = current.next
+    return current.e
+
+  # time complexity: O(n)
   def remove_last(self):
     current = self.head
-
     if current == None:
-      return None
+      raise Exception("list is empty")
     if current.next == None:
-      self.head == None
+      self.head = None
       return current.e
-    
+
     while(current.next.next): 
       current = current.next
     last = current.next
     current.next = None
     return last.e
 
-  def remove_first(self):
-    if self.head == None:
-      return None
-
-    current = self.head
-    self.head = current.next
-    return current.e
-
+  # time complexity: O(1)
   def get_first(self):
-    if self.head:
-      return self.head.e
-    else:
-      return None
+    return self.head.e if self.head else None
 
+  # time complexity: O(n)
   def get_last(self):
     current = self.head
     while current.next:
       current = current.next
     return current.e
 
+  # time complexity: O(n)
   def show_list(self):
+    if self.head == None:
+      return None
+      
     current = self.head
+    print(current.e, end='')
     while current.next:
-      print(f"{current.e}=>", end='')
       current = current.next
-    print(current.e)
+      print(f"=>{current.e}", end='')
 
+  # time complexity: O(n)
+  # basically just a python version of show_list
   def __str__(self):
+    if self.head == None:
+      return None
+
     current = self.head
     return_string = str(current.e)
     while current.next:
       current = current.next
-      return_string += ("=>" + str(current.e))
+      return_string += (f"=>{current.e}")
     return return_string
+
+  # time complexity: O(n)
+  def reverse(self):
+    if self.head == None:
+      raise Exception("list is empty")
+    if self.head.next == None:
+      raise Exception("list only has one element")
+
+    previous = None
+    current = self.head
+    while (current is not None):
+      nxt = current.next
+      current.next = previous
+      previous = current 
+      current = nxt
+    self.head = previous
--- test_main.py
+++ test_main.py
@@ -1,61 +1,65 @@
 import unittest
 from main import *
 
-# Although I didn't use any libraries for Linked_list, I assumed I was allowed to use the unittest library for testing.
+
 
 class Test_Linked_List(unittest.TestCase):
   
   def test_add_first(self):
-    test_list = Linked_List(4)
-
+    test_list = Linked_List()
     test_list.add_first(3)
     self.assertEqual(test_list.get_first(), 3)
-    
-    test_list.add_first("asdf")
-    self.assertEqual(test_list.get_first(), "asdf")
-
-    test_list.add_first(None)
-    self.assertEqual(test_list.get_first(), None)
 
     test_list2 = Linked_List()
     test_list2.add_first(2)
     self.assertEqual(test_list2.get_last(), 2)
+    
+    with self.assertRaises(Exception):
+      test_list.add_first("asdf")
 
   def test_add_last(self):
     test_list = Linked_List()
-
     test_list.add_last(5)
     self.assertEqual(test_list.get_last(), 5)
 
-    test_list.add_last("Hello")
-    self.assertEqual(test_list.get_last(), "Hello")
-
     test_list2 = Linked_List()
     test_list2.add_last(2)
-    self.assertEqual(test_list2.get_last(), 2)
+    self.assertEqual(test_list2.get_first(), 2)
+
+    with self.assertRaises(Exception):
+      test_list.add_last("asdf")
 
   def test_remove_last(self):
-    test_list = Linked_List(4)
-    test_list.add_last(5)
+    test_list = Linked_List()
     test_list.add_last(6)
-    self.assertEqual(test_list.remove_last(), 6)    
+    test_list.add_last(7)
+    self.assertEqual(test_list.remove_last(), 7)    
 
-    test_list2 = Linked_List()
-    test_list2.add_last(2)
-    self.assertEqual(test_list2.remove_last(), 2)
+    with self.assertRaises(Exception):
+      test_list2 = Linked_List()
+      test_list2.get_last()
   
   def test_remove_first(self):
-    test_list = Linked_List(4)
+    test_list = Linked_List()
     test_list.add_last(5)
     test_list.add_last(6)
-    self.assertEqual(test_list.remove_first(), 4)
+    self.assertEqual(test_list.remove_first(), 5)
 
-    test_list2 = Linked_List()
-    self.assertEqual(test_list2.remove_first(), None)
+    with self.assertRaises(Exception):
+      test_list2 = Linked_List()
+      test_list2.remove_first()
+  
+  def test_reverse(self): 
+    test_list = Linked_List()
+    test_list.add_last(1)
+    test_list.add_last(2)
+    test_list.add_last(3)
+    test_list.add_last(4)
 
-    test_list2.add_first(1)
-    self.assertEqual(test_list2.remove_first(), 1)
-
+    test_list.reverse()
+    self.assertEqual(test_list.get_first(), 4)
+    self.assertEqual(test_list.get_last(), 1)
+    self.assertEqual(test_list.__str__(), "4=>3=>2=>1")
 
 if __name__ == '__main__':
   unittest.main()
